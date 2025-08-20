8b377623ef9c2160a49b7a11001b3c4c7287ad17
Bilal Ishtiyaque <bilalishtiyaque1@gmail.com>
2025-08-20 21:30:12 +0500

    restrict request origin in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "*",
+    origin: "https://notes-app-frontend-teal.vercel.app/",
 }));
 
 app.use(express.json());
